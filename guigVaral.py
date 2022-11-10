33360110fe29e8b73544c7271c62fc01d2481afd
FernandoNando2 <ferhdom@gmail.com>
2022-11-10 07:15:44 -0600

    gui para generacion de variables actualizada

--- guigVaral.py
+++ guigVaral.py
@@ -4,28 +4,31 @@ from gVaral import gNorm,gPoisson
 psg.theme('GrayGrayGray')
 
 layout = [
-    [psg.Text('¿Que distribucion deseas generar? ',font = 'Baskerville 20'), psg.InputText(key = 'dist')],
-    [psg.Button('Aceptar',size = (15,1),key = 'aceptar',button_color = ('white','gray'))]
+    [psg.Text('¿Cuantas variables deseas generar?',font = 'Baskerville 20')],
+    [psg.InputText(key = 'num')],
+    [psg.Text('¿Que distribucion deseas generar? ',font = 'Baskerville 20')],
+    [psg.Button('NORMAL',size = (15,1),key = 'normal',button_color = ('white','gray'))
+    ,psg.Button('POISSON',size = (15,1),key = 'poisson',button_color = ('white','gray'))]
 ]
 
 ventana = psg.Window('Simulacion', layout,enable_close_attempted_event = True)
 
 while True:
     event, values = ventana.read()  # type: ignore
-    if event in ('aceptar'):
-        dist = values['dist']
-        if dist == "Normal" or dist == "normal":
-            n = [gNorm(10,6.5) for i in range(100)]
-            with open("Dist.csv", 'w') as d:
-                d.write("Normal, \n")
-                for i in n:
-                    d.writelines(str(i) +", \n")
-        elif dist == "Poisson" or dist == "poisson":
-            p = [gPoisson(5) for i in range(100)]
-            with open("Dist.csv", 'w') as d:
-                d.write("Poisson, \n")
-                for i in p:
-                    d.writelines(str(i) +", \n")
+    if event in ('normal'):
+        num = int(values['num'])
+        n = [gNorm(10,6.5) for i in range(num)]
+        with open("Dist.csv", 'w') as d:
+            d.write("Normal, \n")
+            for i in n:
+                d.writelines(str(i) +", \n")
+    elif event in ('poisson'):
+        num = int(values['num'])
+        p = [gPoisson(5) for i in range(num)]
+        with open("Dist.csv", 'w') as d:
+            d.write("Poisson, \n")
+            for i in p:
+                d.writelines(str(i) +", \n")
     break
 
 ventana.close()
